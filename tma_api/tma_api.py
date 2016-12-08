a75b11c42a63b9f9471b73e3788a4a794e9c9942
Zach Totta <ztotta@gmail.com>
2016-12-07 22:16:28 -0800

    rearranged file structure

---
+++ tma_api/tma_api.py
@@ -0,0 +1,23 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template
+
+import json
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
+with open('../acronymsList.json') as json_data:
+    d = json.loads(json_data.read())
+    
+@app.route('/<abr>')
+def abbreve(abr=None):
+    for i in range(0, len(d)):
+        if d[i]['abbreve'] == abr or d[i]['abbreve'].lower() == abr or d[i]['abbreve'].upper() == abr:
+            abr = d[i]['abbreve']
+            mean = d[i]['meaning']
+            answer = abr + " = " + mean
+            return render_template('success.html', abr=answer)
+    else:
+        return render_template('failure.html')
