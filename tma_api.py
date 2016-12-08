d2bc5909a19fee9ad2ff0b7d15348c4772b634ea
Zach Totta <ztotta@gmail.com>
2016-12-07 23:00:04 -0800

    set port

--- tma_api.py
+++ tma_api.py
@@ -23,4 +23,5 @@ def abbreve(abr=None):
         return render_template('failure.html')
     
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
